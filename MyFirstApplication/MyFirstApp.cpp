ab4664b001dd87bdfa48c2a0a0521a650d59fff9
Nick Germaine <nickwgermaine@gmail.com>
2020-01-02 01:33:09 -0400

    While Loops Part 2

--- MyFirstApplication/MyFirstApp.cpp
+++ MyFirstApplication/MyFirstApp.cpp
@@ -1,29 +1,22 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
-
 int main() {
-	// var 2 is CREATED with value of 200
-	int var2 = 200;
+	int my_var = 10;
 
-	// var 3 is CREATED POINTING to the address space of var2
-	int* var3 = &var2;
+	while (my_var >= 0) {
+		if (my_var == 5) {
+			my_var--;
+			continue;
+		}
 
-	int var4 = var2;
+		if (my_var == 3) {
+			break;
+		}
 
-	// We can now update var 2
-	var2 = 500;
+		std::cout << my_var << std::endl;
+		my_var--;
+	}
 
-	cout << "Var2 Address Location: " << &var2 << endl;
-	cout << "Var4 Value: " << var4 << endl;
-
-	// And when we DEREFERENCE var3 to toggle between displaying
-	// Address space and value AT that address space,
-	// var 3 simply points to the address space of var 2,
-	// thus, getting the NEW and updated value stored at var2
-	cout << "Var3 Value: " << *var3 << endl;
-
-	return 0;
 }
